2a025b7056ac1deda1072292261485eebbd09e33
Fedmich <fedmich@gmail.com>
2014-01-20 15:59:10 +0800

    Use the Shake classes in MainActivity

--- src/com/fedmich/shaketest/MainActivity.java
+++ src/com/fedmich/shaketest/MainActivity.java
@@ -2,15 +2,53 @@ package com.fedmich.shaketest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+
+import com.fedmich.shaketest.ShakeDetectActivity;
+import com.fedmich.shaketest.ShakeDetectActivityListener;
 
 public class MainActivity extends Activity {
 
+	ShakeDetectActivity shakeDetectActivity;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        shakeDetectActivity = new ShakeDetectActivity(this);
+        shakeDetectActivity.addListener(
+        new ShakeDetectActivityListener() {
+            @Override
+            public void shakeDetected() {
+                MainActivity.this.handleShakeEvent();
+            }
+        });
+        
     }
+    
+    void handleShakeEvent( ){
+    	Log.d("Fed", "handShake");
+    }
+    
+    @Override
+	public void onResume() {
+		Log.d("Fed", "/onResume");
+		
+		shakeDetectActivity.onResume();		//Required, resume listening on the sensor
+		super.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		Log.d("Fed", "/onPause");
+		
+		shakeDetectActivity.onPause();	//Required, unregister the sensor
+		super.onPause();
+	}
+	
+	
 
 
     @Override
